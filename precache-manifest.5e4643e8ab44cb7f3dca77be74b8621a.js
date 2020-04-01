01bdc72a9296639ae4c3f30e8a2cad139926c1c8
Aman Sharma <amans7990@gmail.com>
2020-04-01 13:52:54 +0530

    Updates

---
+++ precache-manifest.5e4643e8ab44cb7f3dca77be74b8621a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cb3fea7a957ef6fd3ca5913520adba",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "f8e0189eac5d4ef1d975",
+    "url": "/tic-tac-toe-react/static/css/main.36fa23e5.chunk.css"
+  },
+  {
+    "revision": "e9a6653df4211fe5d31a",
+    "url": "/tic-tac-toe-react/static/js/2.23f31ae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/static/js/2.23f31ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e0189eac5d4ef1d975",
+    "url": "/tic-tac-toe-react/static/js/main.54f195ff.chunk.js"
+  },
+  {
+    "revision": "2ddc285678a387b594e8",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.cd0fb12c.js"
+  }
+]);
